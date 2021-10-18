79370c93ef2a78f4c2d2f43d43a282a9ac94ee35
ChrisWongUK <66286167+TobydhKing@users.noreply.github.com>
2021-10-18 08:46:03 +0100

    feat: Layout, home and book pages

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from flask import render_template
+from flask_bootstrap import Bootstrap
+from flask_nav import Nav
+from flask_nav.elements import *
+
+"""
+#
+logo = ""
+topbar = Navbar(logo,
+                View('Home', 'get_home'),
+                View('Book', 'get_book'),
+                )
+
+# registers the "top" menubar
+nav = Nav()
+nav.register_element('top', topbar)
+"""
+app = Flask(__name__)
+
+
+
+@app.route('/book', methods=['GET'])
+def get_book():
+    return(render_template('book.html'))
+
+
+
+
+@app.route('/home')
+def get_home():
+    return(render_template('home.html'))
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
