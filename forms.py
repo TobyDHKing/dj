5f1c20c170a28c20e0bef502f9bff7723a418686
ChrisWongUK <66286167+TobydhKing@users.noreply.github.com>
2021-10-18 15:55:45 +0100

    feat: Form Object, with validation methods

---
+++ forms.py
@@ -0,0 +1,23 @@
+
+class LoginForm():
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+
+    def validateUsername(self):
+        length = len(self.username)
+        if (not(length < 4)and not(length > 25)):
+            return False
+        else:
+            return True
+
+    def validatePassword(self):
+        length = len(self.password)
+        if (not(length < 4)and not(length > 25)):
+            return False
+        else:
+            return True
+    
+    def validateAll(self):
+        if (self.validatePassword() and self.validateUsername()):
+            return True
